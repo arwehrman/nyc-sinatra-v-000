5d73489519a2ef0d37f3206204c06b219c75adbc
Amanda Wehrman <arwehrman@gmail.com>
2017-10-08 23:43:17 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,14 @@
 class ApplicationController < Sinatra::Base
 
+  configure do
+    enable :sessions
+    set :session_secret, "secret"
+  end
+
   set :views, Proc.new { File.join(root, "../views/") }
   register Sinatra::Twitter::Bootstrap::Assets
+
+  get '/' do
+    erb :index
+  end
 end
--- config/environment.rb
+++ config/environment.rb
@@ -8,6 +8,10 @@ ActiveRecord::Base.establish_connection(
   :database => "db/nyc#{ENV['SINATRA_ENV']}.sqlite"
 )
 
+configure :development do
+  set :database, 'sqlite3:db/database.db'
+end
+
 require_relative "../app/controllers/application_controller.rb"
 
 Dir[File.join(File.dirname(__FILE__), "../app/models", "*.rb")].each {|f| require f}
